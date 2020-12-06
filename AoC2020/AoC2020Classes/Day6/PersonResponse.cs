8122d53962fc5a9e6b2f837f09b98d68c6f8bd00
WaterZoopPlein <28999049+WaterZoopPlein@users.noreply.github.com>
2020-12-06 12:43:44 +0000

    Day 6 Solved (#7)

---
+++ AoC2020/AoC2020Classes/Day6/PersonResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020Classes.Day6
+{
+    public class PersonResponse
+    {
+        public PersonResponse(string rawResponse)
+        {
+            RawResponse = rawResponse;
+        }
+
+        public string RawResponse { get; }
+
+        public List<char> YesQuestionsList => RawResponse.ToCharArray().ToList();
+    }
+}
