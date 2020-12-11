823910211cfaf5e1abf74fe7d73cbed6a56a00fe
WaterZoopPlein <28999049+WaterZoopPlein@users.noreply.github.com>
2020-12-11 20:54:16 +0000

    Day 10 Solved!

--- AoC2020/AoC2020/AoC2020Day10.cs
+++ AoC2020/AoC2020/AoC2020Day10.cs
@@ -11,15 +11,15 @@ namespace AoC2020
         private static readonly List<int> InputList = ReadInput.ConvertInputTextToIntList(@"..\..\..\Inputs\Day10InputText.txt");
 
         private static readonly List<Adapter>
-            ConvertedInputList = Adapter.GenerateSortedListOfAdaptersJoltage(InputList);
+            ConvertedAndSortedAdapterSeriesList = Adapter.GenerateSortedListOfAdaptersJoltage(InputList);
 
         public static void SolvePartOne()
         {
             var diffList = new List<int>();
-            for (var firstIndex = 0; firstIndex < ConvertedInputList.Count - 1; firstIndex++)
+            for (var firstIndex = 0; firstIndex < ConvertedAndSortedAdapterSeriesList.Count - 1; firstIndex++)
             {
-                var firstAdapter = ConvertedInputList[firstIndex];
-                var secondAdapter = ConvertedInputList[firstIndex + 1];
+                var firstAdapter = ConvertedAndSortedAdapterSeriesList[firstIndex];
+                var secondAdapter = ConvertedAndSortedAdapterSeriesList[firstIndex + 1];
 
                 diffList.Add(firstAdapter.OutputJoltage - secondAdapter.OutputJoltage);
             }
@@ -33,7 +33,16 @@ namespace AoC2020
 
         public static void SolvePartTwo()
         {
+            var adapterAndInputAndOutputCounts = ConvertedAndSortedAdapterSeriesList.Count;
 
+            var testStartAdapter = ConvertedAndSortedAdapterSeriesList[0];
+            var testEndAdapter = ConvertedAndSortedAdapterSeriesList[adapterAndInputAndOutputCounts - 1];
+            var testMiddleAdapters =
+                ConvertedAndSortedAdapterSeriesList.GetRange(1, adapterAndInputAndOutputCounts - 2);
+
+            var testResult = AdapterArrangementsSolver.CountAllArrangements(testStartAdapter, testMiddleAdapters, testEndAdapter);
+
+            Console.WriteLine(testResult);
         }
     }
 }
---
+++ AoC2020/AoC2020Classes/Day10/AdapterArrangementsSolver.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020Classes.Day10
+{
+    public class AdapterArrangementsSolver
+    {
+        private static readonly Dictionary<string, long> RecursionStateCache = new Dictionary<string, long>();
+
+        public static long CountAllArrangements(Adapter startAdapter, List<Adapter> middleAdapters, Adapter endAdapter)
+        {
+            long output;
+
+            var currentRecursionState = GetRecursionStateString(startAdapter, middleAdapters, endAdapter);
+
+            if (RecursionStateCache.ContainsKey(currentRecursionState))
+            {
+                return RecursionStateCache[currentRecursionState];
+            }
+
+            if (!middleAdapters.Any())
+            {
+                var startEndDiff = endAdapter.OutputJoltage - startAdapter.OutputJoltage;
+                output = startEndDiff > 3 ? 0 : 1;
+            }
+            else
+            {
+                if (!IsAdapterArrangementLegal(startAdapter, middleAdapters, endAdapter)) return 0;
+
+                var nextLevelMiddleAdapter = middleAdapters.GetRange(0, middleAdapters.Count - 1);
+
+                var leftArrangementsCount = CountAllArrangements(startAdapter, nextLevelMiddleAdapter, endAdapter);
+                var rightArrangementCount = CountAllArrangements(startAdapter, nextLevelMiddleAdapter,
+                    middleAdapters[middleAdapters.Count - 1]);
+
+                output = leftArrangementsCount + rightArrangementCount;
+
+                RecursionStateCache.Add(currentRecursionState, output);
+            }
+
+            return output;
+
+        }
+
+        private static bool IsAdapterArrangementLegal(Adapter startAdapter, List<Adapter> middleAdapters,
+            Adapter endAdapter)
+        {
+            if (middleAdapters[0].OutputJoltage - startAdapter.OutputJoltage > 3)
+            {
+                return false;
+            }
+
+            if (endAdapter.OutputJoltage - middleAdapters[middleAdapters.Count - 1].OutputJoltage  > 3)
+            {
+                return false;
+            }
+
+            for (var firstIndex = 0; firstIndex < middleAdapters.Count - 1; firstIndex++)
+            {
+                var firstAdapter = middleAdapters[firstIndex];
+                var secondAdapter = middleAdapters[firstIndex + 1];
+
+                if (secondAdapter.OutputJoltage - firstAdapter.OutputJoltage > 3)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static string GetRecursionStateString(Adapter startAdapter, List<Adapter> middleAdapters,
+            Adapter endAdapter)
+        {
+            var middleAdaptersString =
+                string.Join(" ", middleAdapters.ConvertAll(item => item.OutputJoltage.ToString()));
+
+            return $"{startAdapter.OutputJoltage} {middleAdaptersString} {endAdapter.OutputJoltage}";
+        }
+    }
+}
