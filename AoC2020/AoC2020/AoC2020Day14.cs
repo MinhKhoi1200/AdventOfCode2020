e645fef82f0ac20e2435d49c63d186e9909d510c
WaterZoopPlein <28999049+WaterZoopPlein@users.noreply.github.com>
2020-12-15 00:25:21 +0000

    Day 14 Solved (#15)

---
+++ AoC2020/AoC2020/AoC2020Day14.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AoC2020Classes.Day14;
+using AoC2020Core;
+
+namespace AoC2020
+{
+    public class AoC2020Day14
+    {
+        private static readonly string[] Delimiter = { Environment.NewLine };
+
+        private static readonly List<string> InputList =
+            ReadInput.ConvertInputTextToStringList(@"..\..\..\Inputs\Day14InputText.txt", Delimiter);
+
+        private static readonly InitProgram InitialiseProgram = new InitProgram(InputList);
+
+        public static void SolvePartOne()
+        {
+            InitialiseProgram.ExecuteV1();
+            Console.WriteLine(InitialiseProgram.MemoryHashTable.Values.ToList().Sum());
+        }
+
+        public static void SolvePartTwo()
+        {
+            // InitialiseProgram.ExecuteV2();
+            InitialiseProgram.ExecuteV2();
+            Console.WriteLine(InitialiseProgram.MemoryHashTable.Values.ToList().Sum());
+        }
+    }
+}
---
+++ AoC2020/AoC2020Classes/Day14/InitProgramUnit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC2020Classes.Day14
+{
+    public class InitProgramUnit
+    {
+        public InitProgramUnit(List<string> rawProgramUnits)
+        {
+            var bitMaskRegex = new Regex(@"(?<=mask\s=\s)[1|0|X]+");
+
+            BitMask = bitMaskRegex.Match(rawProgramUnits[0]).Value;
+
+            var convertedMemoryInstruction = rawProgramUnits.GetRange(1, rawProgramUnits.Count - 1)
+                .Select(rawProgramUnit => new MemoryInstruction(rawProgramUnit)).ToList();
+
+            MemoryInstructions = convertedMemoryInstruction;
+        }
+
+        public string BitMask { get; }
+        public List<MemoryInstruction> MemoryInstructions { get; }
+    }
+}
