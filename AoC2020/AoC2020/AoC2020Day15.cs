5c8da9311005dc3169aa187449920cea4650853d
WaterZoopPlein <28999049+WaterZoopPlein@users.noreply.github.com>
2020-12-15 20:45:08 +0000

    Day 15 Solved (#17)
    * Day 14 to use stringbuilder
    
    * Day 15 Solved!
    
    * Update readme.md

---
+++ AoC2020/AoC2020/AoC2020Day15.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using AoC2020Classes.Day15;
+
+namespace AoC2020
+{
+    public class AoC2020Day15
+    {
+        private static readonly List<int> InputList = new List<int>() {13, 16, 0, 12, 15, 1};
+
+        public static void SolvePartOne()
+        {
+            var memoryChallenge = new MemoryGame(InputList);
+            var testResult = memoryChallenge.Play(2020);
+            Console.WriteLine(testResult);
+        }
+
+        public static void SolvePartTwo()
+        {
+            var memoryChallenge = new MemoryGame(InputList);
+            var testResult = memoryChallenge.Play(30000000);
+            Console.WriteLine(testResult);
+        }
+    }
+}
