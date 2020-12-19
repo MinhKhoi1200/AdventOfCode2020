bfb60b1cb02ca51a2ba4f33a3479716442b4503a
WaterZoopPlein <28999049+WaterZoopPlein@users.noreply.github.com>
2020-12-19 04:19:23 +0000

    Day 18 Solved (#18)

---
+++ AoC2020/AoC2020Classes/Day18/ExpressionUtility.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC2020Classes.Day18
+{
+    public static class ExpressionUtility
+    {
+        public static long EvaluateExpression(string rawExpression, Dictionary<string, int> operatorPrecedence)
+        {
+            var tokens = Tokenise(rawExpression);
+            var rpnExpression = RpnUtility.ConvertToRpn(tokens, operatorPrecedence);
+            var result = RpnUtility.EvaluateRpn(rpnExpression);
+
+            return result;
+        }
+        private static IEnumerable<string> Tokenise(string rawExpression)
+        {
+            var firstRoundConversion = rawExpression.Split(' ').ToList();
+
+            var secondRoundConversion = new List<string>();
+
+            foreach (var item in firstRoundConversion)
+            {
+                secondRoundConversion.AddRange(SeparateBrackets(item));
+            }
+
+            return secondRoundConversion;
+        }
+
+        private static IEnumerable<string> SeparateBrackets(string numberWithBrackets)
+        {
+            if (!(numberWithBrackets.Contains('(') || numberWithBrackets.Contains(')')))
+            {
+                return new List<string> {numberWithBrackets};
+            }
+            var outputList = new List<string>();
+
+            var digitsRegex = new Regex(@"\d+");
+            var openBracketRegex = new Regex(@"\(+");
+            var closeBracketRegex = new Regex(@"\)+");
+
+            var openBrackets = openBracketRegex.Match(numberWithBrackets).Value.ToCharArray().Select(c => c.ToString())
+                .ToList();
+            
+            var closeBrackets = closeBracketRegex.Match(numberWithBrackets).Value.ToCharArray().Select(c => c.ToString())
+                .ToList();
+            
+            var digits = digitsRegex.Match(numberWithBrackets).Value.ToCharArray().Select(c => c.ToString())
+                .ToList();
+
+            outputList.AddRange(openBrackets);
+            outputList.AddRange(digits);
+            outputList.AddRange(closeBrackets);
+
+            return outputList;
+
+
+        }
+    }
+}
---
+++ AoC2020/AoC2020Classes/Day18/RPNUtility.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2020Classes.Day18
+{
+    public static class RpnUtility
+    {
+        internal static IEnumerable<string> ConvertToRpn(IEnumerable<string> rawExpression, Dictionary<string, int> operatorPrecedence)
+        {
+            var outputStack = new Stack<string>();
+
+            var operationStack = new Stack<string>();
+
+            foreach (var currentInputElement in rawExpression)
+            {
+                if (int.TryParse(currentInputElement, out _))
+                {
+                    outputStack.Push(currentInputElement);
+                }
+                else if (IsOperator(operatorPrecedence, currentInputElement))
+                {
+                    if (operationStack.Any())
+                    {
+                        if (operationStack.Peek() != "(")
+                        {
+                            if (operatorPrecedence[currentInputElement] <= operatorPrecedence[operationStack.Peek()])
+                            {
+                                outputStack.Push(operationStack.Pop());
+                            }
+                        }
+
+                        operationStack.Push(currentInputElement);
+                    }
+                    else
+                    {
+                        operationStack.Push(currentInputElement);
+                    }
+                }
+                else
+                    switch (currentInputElement)
+                    {
+                        case "(":
+                            operationStack.Push(currentInputElement);
+                            break;
+                        case ")":
+                        {
+                            while (operationStack.Peek() != "(")
+                            {
+                                var topOperationStack = operationStack.Pop();
+
+                                outputStack.Push(topOperationStack);
+                            }
+
+                            operationStack.Pop();
+                            break;
+                        }
+                    }
+            }
+
+            while (operationStack.Any())
+            {
+                outputStack.Push(operationStack.Pop());
+            }
+
+            return outputStack.ToList();
+
+        }
+
+        internal static long EvaluateRpn(IEnumerable<string> inputRpn)
+        {
+            var resultStack = new Stack<long>();
+            foreach (var currentItem in inputRpn.Reverse())
+            {
+                if (int.TryParse(currentItem, out var result))
+                {
+                    resultStack.Push(result);
+                }
+                else
+                {
+                    var rightNumber = resultStack.Pop();
+                    var leftNumber = resultStack.Pop();
+
+                    switch (currentItem)
+                    {
+                        case "+":
+                            resultStack.Push(leftNumber + rightNumber);
+                            break;
+                        case "*":
+                            resultStack.Push(leftNumber * rightNumber);
+                            break;
+                    }
+                }
+            }
+
+            return resultStack.Pop();
+        }
+        
+        private static bool IsOperator(Dictionary<string, int> operatorPrecedence, string currentInputElement)
+        {
+            return operatorPrecedence.ContainsKey(currentInputElement);
+        }
+    }
+}
